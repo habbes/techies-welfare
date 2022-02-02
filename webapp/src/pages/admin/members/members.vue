b01ef5a30665454b192b0c838c2e2114fcc1e708
Clement Habinshuti <clhabins@microsoft.com>
2022-02-02 22:35:21 +0300

    Refactor admin members layout

--- webapp/src/pages/admin/members/members.vue
+++ webapp/src/pages/admin/members/members.vue
@@ -1,39 +1,57 @@
 <template>
     <div>
-        <ui-h2>Members</ui-h2>
-        <div class="mb-3">
-            <ui-router-button :to="{ name: 'admin-add-member' }">Add new member</ui-router-button>
-        </div>
-        <div class="mb-3">
-            <ui-text-input full placeholder="Search..." v-model="searchTerm"></ui-text-input>
-        </div>
-        <div class="bg-white">
-            <ui-table>
-                <ui-thead>
-                    <ui-th>Name</ui-th>
-                    <ui-th>Phone</ui-th>
-                    <ui-th>Email</ui-th>
-                    <ui-th>Team</ui-th>
-                    <ui-th>Member Since</ui-th>
-                </ui-thead>
-                <ui-tbody>
-                    <ui-tr v-for="user in filteredUsers" :key="user._id">
-                        <ui-td><router-link :to="{ name: 'admin-member-details', params: { id: user._id }}">{{ user.name }}</router-link></ui-td>
-                        <ui-td>{{ user.phone }}</ui-td>
-                        <ui-td>{{ user.email }}</ui-td>
-                        <ui-td>{{ user.team }}</ui-td>
-                        <ui-td>{{ new Date(user.joinedAt||user.createdAt).toDateString() }}</ui-td>
-                    </ui-tr>
-                </ui-tbody>
-            </ui-table>
+        <UiLayout justify="between">
+            <UiLayout>
+                <UiH2>Members</UiH2>
+                <div class="ml-10 w-72">
+                    <UiTextInput full placeholder="Search..." v-model="searchTerm"></UiTextInput>
+                </div>
+            </UiLayout>
+            <div class="">
+                <UiRouterButton :to="{ name: 'admin-add-member' }">Add new member</UiRouterButton>
+            </div>
+            
+        </UiLayout>
+        <div class="">
+            <UiTable>
+                <UiThead>
+                    <UiTH>Name</UiTH>
+                    <UiTH>Phone</UiTH>
+                    <UiTH>Email</UiTH>
+                    <UiTH>Team</UiTH>
+                    <UiTH>Member Since</UiTH>
+                </UiTHead>
+                <UiTBody>
+                    <UiTR v-for="user in filteredUsers" :key="user._id">
+                        <UiTD><router-link :to="{ name: 'admin-member-details', params: { id: user._id }}">{{ user.name }}</router-link></UiTD>
+                        <UiTD>{{ user.phone }}</UiTD>
+                        <UiTD>{{ user.email }}</UiTD>
+                        <UiTD>{{ user.team }}</UiTD>
+                        <UiTD>{{ new Date(user.joinedAt||user.createdAt).toDateString() }}</UiTD>
+                    </UiTR>
+                </UiTBody>
+            </UiTable>
         </div>
     </div>
 </template>
 <script lang="ts">
 import { defineComponent, ref, computed, onMounted } from 'vue'
 import { apiClient } from "../../../api-client";
+import { UiLayout, UiH2, UiTextInput, UiRouterButton, UiTable, UiTH, UiTHead, UiTD, UiTR, UiTBody } from "../../../ui-components";
 
 export default defineComponent({
+    components: {
+        UiLayout,
+        UiH2,
+        UiRouterButton,
+        UiTextInput,
+        UiTable,
+        UiTH,
+        UiTD,
+        UiTR,
+        UiTHead,
+        UiTBody
+    },
     setup() {
         const users = ref([]);
         const searchTerm = ref('');
