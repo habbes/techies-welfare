7cac1dd95944b7a3961b42a101357f8006440f72
Clément Habinshuti <hello@habbes.xyz>
2022-02-20 14:54:08 +0300

    Fix loading user details on mount

--- webapp/src/pages/dashboard/dashboard-home.vue
+++ webapp/src/pages/dashboard/dashboard-home.vue
@@ -15,35 +15,10 @@
     </UiSidebarLayout>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
-import { apiClient } from '../../api-client';
-import { authService } from "../../auth";
-import { useUser } from '../../store';
+import { defineComponent } from 'vue'
 import { UiSidebar, UiSidebarLayout } from "../../ui-components";
 
 export default defineComponent({
-    components: { UiSidebar, UiSidebarLayout },
-    setup() {
-        onMounted(async () => {
-            if (!authService.isAuthenticated()) {
-                console.log("loggin in");
-                try {
-                    await authService.login();
-                    console.log("Login success");
-                }
-                catch (err) {
-                    console.error("Login error", err);
-                }
-            }
-            else {
-                const token = await authService.getAccessToken();
-                const user = await apiClient.getLoggedInUser();
-                useUser().setUser(user);
-                console.log('token', token, 'user', user);
-            }
-
-            
-        })
-    },
+    components: { UiSidebar, UiSidebarLayout }
 })
 </script>
--- webapp/src/pages/dashboard/dashboard-page.vue
+++ webapp/src/pages/dashboard/dashboard-page.vue
@@ -7,18 +7,38 @@
     </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
 import { useRouter } from "vue-router";
 import { UiChevronDownIcon, UiDropdown, UiDropdownItem } from "../../ui-components";
 import NavBar from "../../components/navbar.vue";
 import { authService } from "../../auth";
 import { useUser, clearSession } from "../../store";
+import { apiClient } from '../../api-client';
 
 export default defineComponent({
     components: { NavBar, UiChevronDownIcon, UiDropdown, UiDropdownItem },
     setup() {
         const router = useRouter();
         const user = useUser().user;
+
+        onMounted(async () => {
+            if (!authService.isAuthenticated()) {
+                console.log("loggin in");
+                try {
+                    await authService.login();
+                    console.log("Login success");
+                }
+                catch (err) {
+                    console.error("Login error", err);
+                }
+            }
+            else {
+                const token = await authService.getAccessToken();
+                const user = await apiClient.getLoggedInUser();
+                useUser().setUser(user);
+                console.log('token', token, 'user', user);
+            }
+        });
         
         async function logout() {
             await authService.logout();
